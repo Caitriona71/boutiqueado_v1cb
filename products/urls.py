091297e170e0e7af0420c71560e978cf3af20903
Caitriona71 <caitrionaboyle@ymail.com>
2023-03-28 10:11:15 +0000

    Product Admin - Fixing the image field part 2

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
@@ -8,3 +9,4 @@ urlpatterns = [
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
+
